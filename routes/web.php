41dc43135fcb25e279b01b34cade9b59f1d72608
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2021-12-08 12:34:27 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,30 @@ Route::get('/', function () {
 
 Route::group(['middleware' => 'auth'], function () {
     Route::group(['prefix' => 'pessoas', 'where' => ['id'=>'[0-9]+']], function () {
-        Route::get('',                  [PessoasController::class, 'index']);     
-        Route::get('create',            [PessoasController::class, 'create']);
-        Route::get('store',             [PessoasController::class, 'store']);
-        Route::get('{id}/destroy',      [PessoasController::class, 'destroy']);
-        Route::get('{id}/edit',         [PessoasController::class, 'edit']);
-        Route::get('{id}/update',       [PessoasController::class, 'update']);
+        Route::get('',                  [PessoasController::class, 'index'])->name('pessoas');     
+        Route::get('create',            [PessoasController::class, 'create'])->name('pessoas.create');
+        Route::get('store',             [PessoasController::class, 'store'])->name('pessoas.store');
+        Route::get('{id}/destroy',      [PessoasController::class, 'destroy'])->name('pessoas.destroy');
+        Route::get('{id}/edit',         [PessoasController::class, 'edit'])->name('pessoas.edit');
+        Route::get('{id}/update',       [PessoasController::class, 'update'])->name('pessoas.update');
     });
     
     Route::group(['prefix' => 'entradas', 'where' => ['id'=>'[0-9]+']], function () {
-        Route::any('/',                  [EntradasController::class, 'index']);
-        Route::get('create',            [EntradasController::class, 'create']);
-        Route::get('store',             [EntradasController::class, 'store']);
-        Route::get('{id}/destroy',      [EntradasController::class, 'destroy']);
-        Route::get('{id}/edit',         [EntradasController::class, 'edit']);
-        Route::get('{id}/update',       [EntradasController::class, 'update']);
+        Route::get('/',                 [EntradasController::class, 'index'])->name('entradas');
+        Route::get('create',            [EntradasController::class, 'create'])->name('entradas.create');
+        Route::get('store',             [EntradasController::class, 'store'])->name('entradas.store');
+        Route::get('{id}/destroy',      [EntradasController::class, 'destroy'])->name('entradas.destroy');
+        Route::get('{id}/edit',         [EntradasController::class, 'edit'])->name('entradas.edit');
+        Route::get('{id}/update',       [EntradasController::class, 'update'])->name('entradas.update');
     });
     
     Route::group(['prefix' => 'saidas', 'where' => ['id'=>'[0-9]+']], function () {
-        Route::get('',                  [SaidasController::class, 'index']);
-        Route::get('create',            [SaidasController::class, 'create']);
-        Route::get('store',             [SaidasController::class, 'store']);
-        Route::get('{id}/destroy',      [SaidasController::class, 'destroy']);
-        Route::get('{id}/edit',         [SaidasController::class, 'edit']);
-        Route::get('{id}/update',       [SaidasController::class, 'update']);
+        Route::get('',                  [SaidasController::class, 'index'])->name('saidas');
+        Route::get('create',            [SaidasController::class, 'create'])->name('saidas.create');
+        Route::get('store',             [SaidasController::class, 'store'])->name('saidas.store');
+        Route::get('{id}/destroy',      [SaidasController::class, 'destroy'])->name('saidas.destroy');
+        Route::get('{id}/edit',         [SaidasController::class, 'edit'])->name('saidas.edit');
+        Route::get('{id}/update',       [SaidasController::class, 'update'])->name('saidas.update');
     });
 });
 
